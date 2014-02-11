507c804d78d6d60a149c5308a65cfc99210a950b
Ben Bader <ben@getkeepsafe.com>
2014-02-11 13:48:07 -0800

    Releasing 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject measure "0.1.3-SNAPSHOT"
+(defproject measure "0.1.3"
   :description "Say things about your application with authority, using Coda Hale's Metrics."
   :url "https://github.com/KeepSafe/measure"
   :scm {:name "git"
